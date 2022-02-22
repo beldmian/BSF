b4b689ef76d3f61e236b98726b407aedf5857c9c
Al1en <39908226+Al-1-en@users.noreply.github.com>
2022-02-22 23:39:43 +0300

    excel coords

--- python/parser.py
+++ python/parser.py
@@ -2,25 +2,33 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 import json
+import csv
 
-f = open('./python/bombs.txt', 'a')
+f = open('./python/bombs.csv', 'w')
 
-URL_TEMPLATE = "http://moscow.wikimapia.org/tag/44690/"
-r = requests.get(URL_TEMPLATE)
-print(r.status_code)
+def parse(i):
+    URL_TEMPLATE = "http://moscow.wikimapia.org/tag/44690/" + i
+    r = requests.get(URL_TEMPLATE)
+    print(r.status_code)
 
-soup = bs(r.text, "html.parser")
-vacancies_names = soup.find_all('ul', class_='two-columns clearfix')
-print(len(vacancies_names))
-m = []
-for li in vacancies_names[0]:
-    # li.text
-    a = li.text.split(' - ')
-    if a == ['\n'] or len(a) < 2:
-        continue
-    address, title = a
-    address = address[1:-1:1]
-    title = title[0:-1:1]
-    
+    soup = bs(r.text, "html.parser")
+    vacancies_names = soup.find_all('ul', class_='two-columns clearfix')
+    print(len(vacancies_names))
+
+    for li in vacancies_names[0]:
+        # li.text
+        a = li.text.split(' - ')
+        if a == ['\n'] or len(a) < 2:
+            continue
+        address, title = a
+        address = address[1::1]
+        title = title[0:-1:1]
+        f.write(address + '\n')
+        print(address)
+
+parse('')
+
+for i in range(2, 4):
+    parse(str(i))
 
 f.close()
